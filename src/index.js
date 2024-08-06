65cf7acecb637421ce7b41931da449b748a90c0c
zSqualler <john199502031@gmail.com>
2024-08-06 22:44:40 +0200

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const modulusNumber = Math.abs(n)
+  const array = modulusNumber.toString().split("");
+  const reverseArray = array.reverse();
+  const arrayToString = reverseArray.join('');
+  return +arrayToString;
 }
