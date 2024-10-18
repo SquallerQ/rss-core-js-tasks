8c80d3e86b29f8e61ef31fc53abf8f134ccf8969
zSqualler <john199502031@gmail.com>
2024-10-19 00:31:00 +0200

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+      this.max = null;
+      this.min = null;
+      this.average = null;
     }
-
+    setRange(min, max) {
+      this.min = min
+      this.max = max;
+    }
+    
     guess() {
-
+      this.average = this.calculateAverageNumber(this.min, this.max);
+      return this.average
     }
 
     lower() {
-
+      this.max = this.average;
     }
 
     greater() {
-
+      this.min = this.average;
+    }
+    calculateAverageNumber (minValue, maxValue) {
+      return Math.ceil((minValue + maxValue) / 2);
     }
 }
-
 module.exports = GuessingGame;
