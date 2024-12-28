56498aa504700811897866c38154574066c8fcb4
zSqualler <john199502031@gmail.com>
2024-12-28 23:53:07 +0100

    feat: complete some tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  const newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--double-prev") {
+      if (arr[i - 1]) {
+        newArray.push(arr[i - 1]);
+      }
+    } else if (arr[i] === "--double-next") {
+      if (arr[i + 1]) {
+        newArray.push(arr[i + 1]);
+      } else {
+        i++;
+      }
+    } else if (arr[i] === "--discard-prev") {
+      newArray.pop();
+    } else if (arr[i] === "--discard-next") {
+      if (arr[i + 2] === "--double-prev" || arr[i + 2] === "--discard-prev") {
+        i++;
+      }
+      i++;
+    } else {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 }
 
 module.exports = {
