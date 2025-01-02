0ad7e1cebf482ad4ede9f20c75e076dc031e0e91
zSqualler <john199502031@gmail.com>
2025-01-02 19:35:28 +0100

    feat: complete some tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,30 +1,46 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement chainMaker object according to task description
- * 
- */
 const chainMaker = {
+  array: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.array.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      position < 1 ||
+      position > this.array.length ||
+      position !== Math.floor(position)
+    ) {
+      this.array = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    const firstPart = this.array.slice(0, position - 1);
+    const secondPart = this.array.slice(position, this.array.length);
+    this.array = [...firstPart, ...secondPart];
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = "";
+    for (let i = 0; i < this.array.length; i++) {
+      if (i < this.array.length - 1) {
+        result = result + `( ${this.array[i]} )~~`;
+      } else {
+        result = result + `( ${this.array[i]} )`;
+      }
+    }
+    this.array = [];
+    return result;
+  },
 };
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.value = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  validateValue(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+  modifyKey(message, key) {
+    const keyToUpperCase = key.toUpperCase()
+    let modifiedKey = '';
+    let indexForModify = 0;
+
+    for (const char of message) {
+      if ((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
+        modifiedKey = modifiedKey + keyToUpperCase[indexForModify % keyToUpperCase.length];
+        indexForModify++;
+      } else {
+        modifiedKey = modifiedKey + char;
+      }
+    }   
+    return modifiedKey;
+  }
+  encrypt(message, key) {
+    this.validateValue(message, key)
+
+    const messageToUpperCase = message.toUpperCase();
+    const modifiedKey = this.modifyKey(messageToUpperCase, key);
+    let result = '';
+    
+    for (let i = 0; i < messageToUpperCase.length; i++) {
+      const charMessage = messageToUpperCase[i];
+      const charKey = modifiedKey[i]
+      if (charMessage >= "A" && charMessage <= 'Z') {
+         const encryptedChar = String.fromCharCode(
+          ((charMessage.charCodeAt(0) - 65 + (charKey.charCodeAt(0) - 65)) % 26) + 65
+        );
+        result = result + encryptedChar;
+      } else {
+        result = result + charMessage;
+      }
+    } 
+
+   if (this.value === true) {
+     return result;
+    } else {
+    return result.split('').reverse().join('');
+   }
+  }
+
+
+  
+  decrypt(message, key) {
+    this.validateValue(message, key);
+
+    const messageToUpperCase = message.toUpperCase();
+    const modifiedKey = this.modifyKey(messageToUpperCase, key);
+    let result = '';
+
+    for (let i = 0; i < messageToUpperCase.length; i++) {
+      const charMessage = messageToUpperCase[i];
+      const charKey = modifiedKey[i];
+      if (charMessage >= 'A' && charMessage <= 'Z') {
+        const decryptedChar = String.fromCharCode(
+          ((charMessage.charCodeAt(0) - 65 - (charKey.charCodeAt(0) - 65) + 26) % 26) + 65
+        );
+        result = result + decryptedChar;
+      } else {
+        result = result + charMessage;
+      }
+    }
+    if (this.value === true) {
+      return result;      
+    } else {
+      return result.split('').reverse().join('')
+    }
   }
 }
 
