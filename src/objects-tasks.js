f79fe3eaaa0948eaad3e2d2cc81de1dc5d14284e
zSqualler <john199502031@gmail.com>
2025-03-10 20:21:04 +0100

    feat: complete last tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -138,8 +138,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const wordArray = [];
+  Object.entries(lettersObject).forEach(([letter, positions]) => {
+    positions.forEach((position) => {
+      wordArray[position] = letter;
+    });
+  });
+
+  return wordArray.join('');
 }
 
 /**
@@ -156,8 +163,42 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let count25 = 0;
+  let count50 = 0;
+
+  const canSellAllTickets = queue.every((bill) => {
+    switch (bill) {
+      case 25:
+        count25 += 1;
+        return true;
+
+      case 50:
+        if (count25 > 0) {
+          count25 -= 1;
+          count50 += 1;
+          return true;
+        }
+        return false;
+
+      case 100:
+        if (count50 > 0 && count25 > 0) {
+          count50 -= 1;
+          count25 -= 1;
+          return true;
+        }
+        if (count25 >= 3) {
+          count25 -= 3;
+          return true;
+        }
+        return false;
+
+      default:
+        return false;
+    }
+  });
+
+  return canSellAllTickets;
 }
 
 /**
