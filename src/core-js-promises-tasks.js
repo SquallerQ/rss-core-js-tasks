3d7bb45863bb4a39f7e4885fe4294a882c507005
zSqualler <john199502031@gmail.com>
2025-03-29 12:11:29 +0100

    init: start promises-task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const result = new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject(number);
+    } else {
+      resolve(number);
+    }
+  });
+  return result;
 }
 
 /**
